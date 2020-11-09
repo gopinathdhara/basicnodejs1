a5cda2cba759a129dd34153c6e35b7bea6e2eda0
gopi12 <gopi1.dhara@gmail.com>
2020-11-09 23:07:48 +0530

    final chg2

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const express = require('express');
 const path = require('path');
 const PORT = process.env.PORT || 5000;
 
+
 express()
-  .get('/', (req, res) => res.send('Welcome to Basic node express app starting !')  )
+  .get('/', (req, res) => res.send('Welcome to Basic node express app starting now !')  )
   .get('/cool', (req, res) => res.send(cool()))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`));
