64f3f012b35a50b593546b5319406db8636bd8d7
gopi12 <gopi1.dhara@gmail.com>
2020-11-09 22:46:27 +0530

    final chg

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
-var express = require('express');  
-var app = express();  
-app.get('/', function (req, res) {  
-  res.send('Welcome to Basic node express app !');  
-});  
-var server = app.listen(process.env.PORT || 8000, function () {  
-  var host = server.address().address;  
-  var port = server.address().port;  
-  console.log('Example app listening at http://%s:%s', host, port);  
-});  
+const cool = require('cool-ascii-faces');
+const express = require('express');
+const path = require('path');
+const PORT = process.env.PORT || 5000;
+
+express()
+  .get('/', (req, res) => res.send('Welcome to Basic node express app !')  )
+  .get('/cool', (req, res) => res.send(cool()))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
