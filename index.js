9fc54dc291a4a705ecaf79dff55f477c10ec4f7d
gopi12 <gopi1.dhara@gmail.com>
2020-11-10 06:00:16 +0530

    cors

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const middlewares = require('./middlewares');
 var cors = require('cors'); 
 app.use(cors({credentials: true, origin: settings.APIUrl}));
 app.use(function(req, res, next) {
-  //res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With,Content-Type,accesstoken,regenarate-token,Accept,enctype,contentType,processData,cache');
   res.setHeader('Access-Control-Expose-Headers','Content-Type,expire,Accept');
